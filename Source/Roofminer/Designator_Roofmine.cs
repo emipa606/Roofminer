70edd2feef4413e94aba7f39c5ffd0a9f3f932ca
emipa606 <emil.offermo@litet.net>
2021-07-13 18:59:42 +0200

    Mod updated for 1.3 and passed autotests

--- Source/Roofminer/Designator_Roofmine.cs
+++ Source/Roofminer/Designator_Roofmine.cs
@@ -1,39 +1,36 @@
 using System.Collections.Generic;
 using RimWorld;
-using Verse;
 using UnityEngine;
+using Verse;
 
 namespace Roofminer
 {
     public class Designator_Roofmine : Designator_Mine
     {
-
-        public override int DraggableDimensions => 0;
-
         public Designator_Roofmine()
         {
             defaultLabel = "DesignatorRoofmine".Translate();
-            icon = ContentFinder<Texture2D>.Get("Designators/Roofmine", true);
+            icon = ContentFinder<Texture2D>.Get("Designators/Roofmine");
             defaultDesc = "DesignatorRoofmineDesc".Translate();
         }
 
+        public override int DraggableDimensions => 0;
+
         public override void DesignateSingleCell(IntVec3 loc)
         {
-
-            RoofDef originalLocRoof = Map.roofGrid.RoofAt(loc);
+            var originalLocRoof = Map.roofGrid.RoofAt(loc);
             // tiles are added to a queue to ensure we process closer tiles first
-            Queue<IntVec3> locQueue = new Queue<IntVec3>();
+            var locQueue = new Queue<IntVec3>();
             // remember every tile we have queued, to avoid duplicating effort
-            HashSet<IntVec3> locQueued = new HashSet<IntVec3>();
+            var locQueued = new HashSet<IntVec3>();
 
             locQueue.Enqueue(loc);
             locQueued.Add(loc);
 
-            int numDesignated = 0;
+            var numDesignated = 0;
 
             while (locQueue.Count > 0 && numDesignated < 1201)
             {
-
                 // Log.Message("Deqeueing " + loc.ToString());
                 loc = locQueue.Dequeue();
 
@@ -41,40 +38,58 @@ namespace Roofminer
                 Thing locThing = loc.GetFirstMineable(Map);
                 // Log.Message("checking locThing==null");
                 if (locThing == null)
+                {
                     continue;
+                }
+
                 // Log.Message("checking CanDesignateThing");
                 if (!CanDesignateThing(locThing).Accepted)
+                {
                     continue;
+                }
+
                 // Log.Message("getting locRoof");
-                RoofDef locRoof = Map.roofGrid.RoofAt(loc);
+                var locRoof = Map.roofGrid.RoofAt(loc);
                 // Log.Message("checking one null roof");
                 if (locRoof == null && originalLocRoof != null ||
-                        locRoof != null && originalLocRoof == null)
+                    locRoof != null && originalLocRoof == null)
+                {
                     continue;
+                }
+
                 // Log.Message("checking roof defNames match");
-                if (locRoof != null && originalLocRoof != null &&
-                        locRoof.defName != originalLocRoof.defName)
+                if (locRoof != null &&
+                    locRoof.defName != originalLocRoof.defName)
+                {
                     continue;
+                }
+
                 // Log.Message("checking DesignationAt");
                 if (Map.designationManager.DesignationAt(loc, DesignationDefOf.Mine) != null)
+                {
                     continue;
+                }
 
                 // Log.Message("Designating " + loc.ToString());
                 base.DesignateSingleCell(loc);
                 numDesignated++;
 
-                foreach (IntVec3 newLoc in GenAdjFast.AdjacentCellsCardinal(loc))
+                foreach (var newLoc in GenAdjFast.AdjacentCellsCardinal(loc))
                 {
                     if (!newLoc.InBounds(Map))
-                        continue;
-                    if (!locQueued.Contains(newLoc))
                     {
-                        // Log.Message("Enqueueing " + newLoc.ToString());
-                        locQueue.Enqueue(newLoc);
-                        locQueued.Add(newLoc);
+                        continue;
                     }
-                }
 
+                    if (locQueued.Contains(newLoc))
+                    {
+                        continue;
+                    }
+
+                    // Log.Message("Enqueueing " + newLoc.ToString());
+                    locQueue.Enqueue(newLoc);
+                    locQueued.Add(newLoc);
+                }
             }
         }
 
@@ -82,26 +97,21 @@ namespace Roofminer
         {
             base.DrawMouseAttachments();
             var mouseCell = UI.MouseCell();
-            if (mouseCell == null || !mouseCell.InBounds(Map) || mouseCell.Fogged(Map))
+            if (!mouseCell.InBounds(Map) || mouseCell.Fogged(Map))
             {
                 return;
             }
+
             if (mouseCell.GetFirstMineable(Map) == null)
             {
                 return;
             }
+
             var currentRoof = Map.roofGrid.RoofAt(mouseCell);
-            string toolTip;
-            if (currentRoof == null)
-            {
-                toolTip = "Unroofed";
-            }
-            else
-            {
-                toolTip = currentRoof.label.CapitalizeFirst();
-            }
+            var toolTip = currentRoof == null ? "Unroofed" : currentRoof.label.CapitalizeFirst();
+
             var mousePosition = Event.current.mousePosition;
-           Rect rect = new Rect(mousePosition.x + 29f, mousePosition.y, 999f, 29f);
+            var rect = new Rect(mousePosition.x + 29f, mousePosition.y, 999f, 29f);
             Text.Font = GameFont.Small;
             Text.Anchor = TextAnchor.MiddleLeft;
             Widgets.Label(rect, toolTip);
